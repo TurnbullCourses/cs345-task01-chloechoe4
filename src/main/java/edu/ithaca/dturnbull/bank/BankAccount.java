e8c49c08c5b835cdc089ac156816faff35cdc64e
ellen6820 <echapman@ithaca.edu>
2022-01-30 11:20:53 -0500

    Withdraw implementation

--- src/main/java/edu/ithaca/dturnbull/bank/BankAccount.java
+++ src/main/java/edu/ithaca/dturnbull/bank/BankAccount.java
@@ -26,32 +26,51 @@ public class BankAccount {
         return email;
     }
 
+
+    class InvalidAmountException extends Exception{
+        public InvalidAmountException(String message)
+        {
+           message = "Please enter a positive amount to withdraw";
+        }
+    }
+
     /**
      * @post reduces the balance by amount if amount is non-negative and smaller than balance
      */
-    public void withdraw (double amount) throws InsufficientFundsException{
+    public void withdraw (double amount) throws InsufficientFundsException, InvalidAmountException{
         if (amount <= balance){
             balance -= amount;
         }
-        else {
+        else if (amount >= balance){
             throw new InsufficientFundsException("Not enough money");
+        }
+        else{
+            throw new InvalidAmountException("Please enter a positive amount to withdraw");
         }
     }
 
 
     public static boolean isEmailValid(String email){
-        
+
         if (email.indexOf('@') == -1){
             return false;
         }
+        
         int beforeAt = email.indexOf("@") - 1;
-        if(email.contains("#")){
+        String[] check = email.split("@");
+        String prefix = check[0];
+        String domain = check[1];
+
+        if(prefix.contains("#")){
             return false;
         }
         else if(email.contains("..")){
             return false;
         }
         else if(email.indexOf(".") == beforeAt | email.indexOf("_") == beforeAt | email.indexOf("-") == beforeAt){
+            return false;
+        }
+        else if(!domain.contains(".")){
             return false;
         }
         else if(email.length() - email.indexOf(".") < 3){
